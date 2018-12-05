b7719dba2d87a896bb4e0712fc7c6b9fa9737370
Madeline Carr <Carrm2@vcu.edu>
2018-12-04 21:26:30 -0500

    Constructor Property
    I added examples of the constructor property.

--- review.js
+++ review.js
@@ -33,3 +33,17 @@ function AnotherCircle(radius) {
 }
 const another = new AnotherCircle(8);
 another.draw();
+
+//Constructor Property
+console.log(circle.constructor); //Object 
+console.log(newCircle.constructor); //Object 
+console.log(another.constructor); //AnotherCircle 
+
+var boolean = true;
+console.log(boolean.constructor); //Boolean
+
+var number = 8;
+console.log(number.constructor); //Number
+
+var array = [1, 2, 3, 4, 5];
+console.log(array.constructor); //Array
